f4d6a0fcd647e62a8fb448fcf05da824e5715294
David Shonn <davidlshonn@icloud.com>
2021-03-20 21:53:27 +0000

    install and activate added

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,3 +1,4 @@
+//Files to cache
 const FILES_TO_CACHE = [
   "/",
   "/db.js",
@@ -8,5 +9,38 @@ const FILES_TO_CACHE = [
   "/icons/icon-512x512.png",
 ];
 
+//Assign Names
 const CACHE_NAME = "static-cache";
 const DATA_CACHE_NAME = "data-cache";
+
+//Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+//Activate
+self.addEventListener("activate", (event) => {
+  const currentCaches = [CACHE_NAME, DATA_CACHE_NAME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter(
+          (cacheName) => !currentCaches.includes(cacheName)
+        );
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
