1e448b8ad3ea05e51d455a6f4ac9c7b2e9d1160f
David Shonn <davidlshonn@icloud.com>
2021-03-20 22:32:47 +0000

    Use cached and request if not cached added

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -67,3 +67,12 @@ self.addEventListener("fetch", (event) => {
     return;
   }
 });
+
+//Return
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
