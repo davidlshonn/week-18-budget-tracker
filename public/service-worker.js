00c22245cd4d194a334ec9a589a7ab553fc796a3
David Shonn <davidlshonn@icloud.com>
2021-03-21 15:34:41 +0000

    Bug fixed, application now fully functional

--- public/service-worker.js
+++ public/service-worker.js
@@ -46,42 +46,36 @@ self.addEventListener("activate", (event) => {
 });
 
 //Fetch
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", function(event) {
+  // cache all get requests to /api routes
   if (event.request.url.includes("/api/")) {
     event.respondWith(
-      caches.open(DATA_CACHE_NAME).then(
-        ((cache) => {
-          return fetch(event.request).then(
-            ((response) => {
-              if (response.status === 200) {
-                cache.put(event.request.url, response.clone());
-              }
-              return response;
-            }).catch((err) => {
-              return cache.match(event.request);
-            })
-          );
-        }).catch((err) => console.log(err))
-      )
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
     );
     return;
   }
-});
-
-//Use cache initially for all othe requests
-self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      // If request is not in cache. make network request and cache the response
-      return caches.open(DATA_CACHE_NAME).then((cache) => {
-        return fetch(event.request).then((response) => {
-          return cache.put(event.request, response.clone()).then(() => {
-            return response;
-          });
-        });
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
       });
     })
   );
